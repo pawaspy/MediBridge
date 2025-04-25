d3764f2f53d615a02190e3ec1283521079416551
pawaspy <pawaspy2633@gmail.com>
2025-04-25 12:43:08 +0000

    add the config file

--- util/config.go
+++ util/config.go
@@ -1,2 +1,20 @@
 package util
 
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBSource string `mapstructure:"DB_SOURCE"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+	viper.AutomaticEnv()
+
+	if err = viper.ReadInConfig(); err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
