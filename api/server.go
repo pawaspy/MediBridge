13f9055929315cb57f17aa17f1d94a846ec06e84
pawaspy <pawaspy2633@gmail.com>
2025-04-25 16:17:50 +0000

    add the store and server file

---
+++ api/server.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/pawaspy/MediBridge/db/sqlc"
+	"github.com/pawaspy/MediBridge/util"
+)
+
+type Server struct {
+	config util.Config
+	store db.Store
+	router *gin.Engine
+}
+
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	server := &Server{
+		config: config,
+		store: store,
+	}
+
+	server.setupRouter()
+
+	return server, nil
+}
+
+func (server *Server) setupRouter() {
+	router := gin.Default()
+
+	router.POST("")
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- api/user.go
+++ api/user.go
@@ -1,2 +1,13 @@
 package api
 
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Role     string `json:"role" binding:"required"`
+}
+
+type userResponse struct{
+	
+}
